8924e22f2c9ea89606ac71cfe665c6b3ec2977c3
mecorre1 <mecorre1@gmail.com>
2024-12-28 18:58:04 +0100

    Add rooms and devices

---
+++ rooms.js
@@ -0,0 +1,8 @@
+// room : [devices]
+// used BlueLight app to indetify each by proximity with my phone
+{
+    "kitchen": ["CC:22:37:11:26:4F","CC:22:37:11:30:EC"],
+    "living_room": ["CC:22:37:11:5D:02"],
+    "bedroom_1": ["CC:22:37:11:1E:40"],
+    "bedroom_2": ["CC:22:37:11:1E:84"]
+  }
